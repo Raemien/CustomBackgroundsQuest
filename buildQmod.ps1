a577dc257ff7b359bcba5eb90550d59a70d93c99
Raemien <39755457+Raemien@users.noreply.github.com>
2021-05-02 20:47:31 +1200

    Shift everything to Qmod schema
    ...and thus ends the tale of bmbfmod.json.

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -7,8 +7,9 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
-Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so",`
+Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_3_3.so",`
                         "./libs/arm64-v8a/libcustombgs.so",`
+                        "./cover.png",`
                         "./mod.json" -DestinationPath "./CustomBackgrounds.zip" -Update
 
 & copy-item -Force "./CustomBackgrounds.zip" "./CustomBackgrounds.qmod"
