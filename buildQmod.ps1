d264267d0890fdfc9924b8c048db62f6bd631e92
Raemien <39755457+Raemien@users.noreply.github.com>
2021-08-19 09:45:00 +1200

    Bump to 1.16.4 dependencies

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -1,4 +1,4 @@
-# Builds a .zip file for loading with BMBF
+# Builds a .zip file for loading with BMBF/QP
 $NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
 $buildScript = "$NDKPath/build/ndk-build"
@@ -7,7 +7,7 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
-Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_3_3.so",`
+Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so",`
                         "./libs/arm64-v8a/libcustombgs.so",`
                         "./cover.png",`
                         "./mod.json" -DestinationPath "./CustomBackgrounds.zip" -Update
