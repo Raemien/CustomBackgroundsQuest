60e9cdc044f3845772a908dda912d01bd11d45bc
Raemien <39755457+Raemien@users.noreply.github.com>
2021-09-04 12:16:14 +1200

    Version bump for 1.17.0

--- buildQmod.ps1
+++ buildQmod.ps1
@@ -7,9 +7,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
-Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so",`
+Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so",`
                         "./libs/arm64-v8a/libcustombgs.so",`
                         "./cover.png",`
                         "./mod.json" -DestinationPath "./CustomBackgrounds.zip" -Update
-
 & copy-item -Force "./CustomBackgrounds.zip" "./CustomBackgrounds.qmod"
